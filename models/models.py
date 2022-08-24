ce289b8ccde3b3d7a25d6a3572c3d72bf33f3cd6
MMnaimi <mm.naimi@yahoo.com>
2022-08-24 18:32:02 +0430

    Show active and non-active records, Replace 'create and edit' from views

--- models/models.py
+++ models/models.py
@@ -8,24 +8,38 @@ class Student(models.Model):
     _description = 'Student'
     _rec_name = 'first_name'
 
-    first_name = fields.Char(states={'draft': [('readonly', False)]}, readonly=True)
-    last_name  = fields.Char(states={'draft': [('readonly', False)]}, readonly=True)
-    father_name = fields.Char(states={'draft': [('readonly', False)]}, readonly=True)
-    age = fields.Integer(states={'draft': [('readonly', False)]}, readonly=True)
+    first_name = fields.Char()
+    last_name  = fields.Char()
+    father_name = fields.Char()
+    age = fields.Integer()
+    active = fields.Boolean(default=True)
     gender = fields.Selection(
-        selection=[('male', 'Male'), ('female', 'Female')],
-        states={'draft': [('readonly', False)]}, 
-        readonly=True
+        selection=[('male', 'Male'), ('female', 'Female')]
     )
     state = fields.Selection(
         selection=[('draft', 'Draft'), ('active', 'Active')],
-        default='draft',
-        states={'draft': [('readonly', False)]}, 
-        readonly=True
+        default='draft'
     )
+    teacher_id = fields.Many2one('teacher.teacher')
 
     @api.constrains('state')
     def check_state(self):
         for rec in self:
             if rec.age < 7 and rec.state == 'active':
                 raise ValidationError("Not qualified")
+
+
+
+class Teacher(models.Model):
+    _name = 'teacher.teacher'
+    _description = 'Teacher'
+    _rec_name = 'first_name'
+
+    first_name = fields.Char(required=True)
+    last_name = fields.Char()
+    age = fields.Integer()
+    gender = fields.Selection(
+        selection=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')],
+        default='male'
+    )
+    student_id = fields.One2many('student.student', 'teacher_id')
